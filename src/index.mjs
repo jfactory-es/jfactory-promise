8a562ff926e5d5cb0cf7401390586934284c5a43
jfactory-es <sp.jfactory@gmail.com>
2020-09-07 12:22:13 +0200

    1.1.0

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import { jFactoryLoader } from "../../../jFactory_1/workspace/src/jFactory-loader";
-export { JFactoryPromise } from "../../../jFactory_1/workspace/src/JFactoryPromise.mjs";
+export { JFactoryPromise } from "../../../jFactory_1/workspace/src/JFactoryPromise";
 
 if (typeof jFactoryOverride === "undefined" || !jFactoryOverride) {
     jFactoryLoader.init();
