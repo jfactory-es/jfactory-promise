37a13086e9f06a37d4460ded760c7a17166bd446
jfactory-es <sp.jfactory@gmail.com>
2021-01-11 09:44:48 +0100

    1.7.7

--- dist/JFactoryPromise-devel.mjs.js
+++ dist/JFactoryPromise-devel.mjs.js
@@ -1,5 +1,5 @@
 /*!
- * JFactoryPromise v1.7.7-beta.2
+ * JFactoryPromise v1.7.7
  * http://github.com/jfactory-es/jfactory-promise
  * (c) 2019-2021, Stéphane Plazis, http://github.com/jfactory-es/jfactory-promise/blob/master/LICENSE.txt
  */
@@ -13,7 +13,7 @@ import _ from 'lodash';
 
 // The builder replaces these lines
 const JFACTORY_NAME = "JFactoryPromise";
-const JFACTORY_VER  = "1.7.7-beta.2-devel-mjs";
+const JFACTORY_VER  = "1.7.7-devel-mjs";
 
 // The builder may replace env("JFACTORY_ENV_*") by hard coded true/false primitives,
 // allowing the bundler to remove unused code using Tree Shaking
